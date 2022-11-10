6843242e6e3656f6a808b0d7ea674b13acee54d1
PQNDAAA <gayvallet.dylan@gmail.com>
2022-11-10 14:05:10 +0100

    Update for bugs

--- Assets/Scripts/SearchSystem.cs
+++ Assets/Scripts/SearchSystem.cs
@@ -8,13 +8,7 @@ public class SearchSystem : MonoBehaviour
     public InputField searchField;
     public Text Answers;
     public Utilities utils;
-    void Start()
-    {
-    }
-    void Update()
-    {
 
-    }
     public void OnClickHandler()
     {
         utils.checkSearchField(searchField, Answers);
