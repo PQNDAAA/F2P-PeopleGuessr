96f83d5ac75a81917b03fe081072f4e060426fff
PQNDAAA <gayvallet.dylan@gmail.com>
2022-11-13 00:05:50 +0100

    Bugs fixed and a features update

--- Assets/Scripts/PeoplesList.cs
+++ Assets/Scripts/PeoplesList.cs
@@ -8,8 +8,8 @@ public class PeoplesList : MonoBehaviour
 
     public static List<CreatePeoplesList> peoplelist = new List<CreatePeoplesList>
     {
-        new CreatePeoplesList(0,"Jean","null",45,1.75f,"Espana"),
-        new CreatePeoplesList(1,"Gerard","null",26,1.86f,"Japan"),
-        new CreatePeoplesList(2,"Charline","null",20,1.66f,"Argentina")
+        new CreatePeoplesList(0,"Jean","Doctor",45,1.75f,"Espana"),
+        new CreatePeoplesList(1,"Gerard","Mathematician",36,1.86f,"Japan"),
+        new CreatePeoplesList(2,"Charline","Lawyer",20,1.66f,"Argentina")
     };
 }
--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -9,6 +9,7 @@ public class Utilities : MonoBehaviour
     public Text peopleRandomTxt;
     public GameObject GameUI;
     public GameObject WinUI;
+    Dictionary<string, object> peopleRandomInfo = new Dictionary<string, object>();
 
     public void RandomPeople()
     {
@@ -16,17 +17,28 @@ public class Utilities : MonoBehaviour
         Debug.Log(random.Name);
         peopleRandomTxt.text = random.Name;
 
-    }
+        foreach (CreatePeoplesList people in PeoplesList.peoplelist)
+        {
+            if (people.Name == peopleRandomTxt.text)
+            {
+                peopleRandomInfo.Add("Name", people.Name);
+                peopleRandomInfo.Add("Job", people.Job);
+                peopleRandomInfo.Add("Age", people.Age);
+
+            }
+        }
+     }
 
     public void checkSearchField(InputField search, Text answerslist)
     {
-        if(search != null)
+        if(search.text != null && !answerslist.text.Contains(search.text))
         {
             foreach(CreatePeoplesList people in PeoplesList.peoplelist)
             {
                 if(people.Name == search.text)
                 {
                     answerslist.text += people.Name + " " + people.Job + " " + people.Size + " " + people.Age + "\n";
+                    compareCorrectPerson(search);
                 }
                 
                 if(search.text == peopleRandomTxt.text)
@@ -39,6 +51,43 @@ public class Utilities : MonoBehaviour
         }
         
         search.text = "";
+    }
+
+    public void compareCorrectPerson(InputField search)
+    {
+        var age = peopleRandomInfo["Age"];
+
+        foreach (CreatePeoplesList people in PeoplesList.peoplelist)
+        {
+            if (people.Name.Contains(search.text)) 
+            {
+                if (peopleRandomInfo.ContainsValue(people.Name))
+                {
+                    Debug.Log("The name " + search.text + " is correct");
+                }
+                else
+                {
+                    Debug.Log("The name " + search.text + " isn't correct");
+                }
+                if(peopleRandomInfo.ContainsValue(people.Age))
+                {
+                    Debug.Log(people.Age + " == " + age);
+                } else
+                {
+                    Debug.Log(people.Age + " != " + age);
+                }
+                if (peopleRandomInfo.ContainsValue(people.Job))
+                {
+                    Debug.Log("It's the correct job.");
+                }
+                else
+                {
+                    Debug.Log("It's not the correct job.");
+                }
+            }
+         
+
+        }
     }
 
     public void BackToMainMenu()
