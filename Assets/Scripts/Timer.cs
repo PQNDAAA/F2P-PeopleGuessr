0ee117c66e2efb68d594b499e2fbdf562404f903
PQNDAAA <gayvallet.dylan@gmail.com>
2022-11-16 12:03:57 +0100

    Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float minutes = 0;
+    public float seconds = 0;
+
+    public bool TimerIsRunning = false;
+
+    Text timerTxt;
+
+    void Start()
+    { 
+      timerTxt = GetComponent<Text>();
+
+      TimerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (TimerIsRunning)
+        {
+            if (seconds > 0)
+            {
+                seconds -= Time.deltaTime;
+            }
+            else
+            {
+                if(minutes == 0)
+                {
+                    TimerIsRunning=false;
+                    Debug.Log("Timer is done");
+                }
+                else
+                {
+                    minutes -= 1;
+                    seconds = 59;
+                }
+            }
+            timerTxt.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+    }
+}
