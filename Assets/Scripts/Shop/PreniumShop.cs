5d1321dd55351009bab18c922d01e7bbeb23b92d
PQNDAAA <gayvallet.dylan@gmail.com>
2022-12-10 18:41:44 +0100

    Update "Shop System" 0.4.2

--- Assets/Scripts/Shop/PreniumShop.cs
+++ Assets/Scripts/Shop/PreniumShop.cs
@@ -7,7 +7,8 @@ using UnityEngine.UI;
 public class PreniumShop : MonoBehaviour
 {
     public enum numberOfCoins { _100 = 100, _200 = 200, _300 = 300, _400 = 400, 
-        _500 = 500, _600 = 600, _700 = 700, _800 = 800, _900 = 900, _1000= 1000}
+        _500 = 500, _600 = 600, _700 = 700, _800 = 800, _900 = 900, _1000= 1000,_2400 = 2400,
+    _4875 = 4875,_6200 = 6200, _8250 = 8250,_11000 = 11000}
     public numberOfCoins coinsPrice;
 
     public Text numberOfCoinsText;
@@ -18,7 +19,7 @@ public class PreniumShop : MonoBehaviour
     private void Start()
     {
         int value = (int)coinsPrice;
-        numberOfCoinsText.text = value.ToString();
+        numberOfCoinsText.text = value.ToString() + " COINS";
 
         coinsManager = GetComponent<CoinsManager>();    
     }
